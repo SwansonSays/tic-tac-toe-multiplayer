eebf4ebf1d4cee039e9b19d6b45cc0094eedcbe1
SwansonSays <rpswanson18@gmail.com>
2024-08-07 19:12:31 -0700

    Added api names for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const server = createServer(app);
@@ -10,37 +11,28 @@ const io = new Server(server, {
     }
 });
 
-const names = [];
-const rooms = [];
+const games = {};
 
 io.on('connection', async (socket) => {
     console.log('a user connected');
-    const sockets = await io.fetchSockets();
-    //console.log(sockets);
-    console.log("sockets: " + socket.rooms);
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
-    socket.on('new player', (name) => {
-        socket.data.username = name;
-        names.push(name);
-        console.log('Player registered with name: ' + name);
-        //check for open rooms
-        if(rooms > 0) {
+    socket.on('create room', () => {
 
-        } else {
-            socket.join(name.concat("-room"));
-            console.log(socket.rooms);
-            console.log("socket: " + socket.id + " username: " + socket.data.username + " Joined room: " + socket.id)
-        }
-        console.log(io.fetchSockets());
     });
 
-    socket.on('start game', () => {
-        console.log("startgame called");
+    socket.on('join room', () => {
+
     });
+
+    socket.on('move', (roomId, move) => {
+
+    });
+
+
 });
 
 server.listen(4000, () => {
