c3489e1e611694a429c8da2e528bb452f1595b75
SwansonSays <rpswanson18@gmail.com>
2024-08-07 19:03:42 -0700

    installed uuid to create unique room names

--- server/index.js
+++ server/index.js
@@ -10,15 +10,36 @@ const io = new Server(server, {
     }
 });
 
+const names = [];
+const rooms = [];
+
 io.on('connection', async (socket) => {
     console.log('a user connected');
+    const sockets = await io.fetchSockets();
+    //console.log(sockets);
+    console.log("sockets: " + socket.rooms);
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
     socket.on('new player', (name) => {
-        console.log('user created name: ' + name);
+        socket.data.username = name;
+        names.push(name);
+        console.log('Player registered with name: ' + name);
+        //check for open rooms
+        if(rooms > 0) {
+
+        } else {
+            socket.join(name.concat("-room"));
+            console.log(socket.rooms);
+            console.log("socket: " + socket.id + " username: " + socket.data.username + " Joined room: " + socket.id)
+        }
+        console.log(io.fetchSockets());
+    });
+
+    socket.on('start game', () => {
+        console.log("startgame called");
     });
 });
 
